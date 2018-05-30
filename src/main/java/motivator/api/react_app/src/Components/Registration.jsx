659cab91518272ea7977cca423c33fcf5c51d0e7
Attila Nagy <attila.nagy880@gmail.com>
2018-05-30 15:02:22 +0200

    i like turtles no2

--- src/main/java/motivator/api/react_app/src/Components/Registration.jsx
+++ src/main/java/motivator/api/react_app/src/Components/Registration.jsx
@@ -27,47 +27,49 @@ class Create extends Component {
 
     axios.post(`http://127.0.0.1:8080/register`, { name, password, email, gitHubProfile, trelloProfile, slackProfile })
       .then((result) => {
-        this.props.history.push("/")
+        if (result.status == '200') {
+          this.$router.push('/login');
+        }
       });
   }
 
   render() {
     const { name, password, email, gitHubProfile, trelloProfile, slackProfile } = this.state;
     return (
-      <div class="container">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">
-              ADD PROFILE
+      <div className="container">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">
+              Registrate new profile.
             </h3>
           </div>
-          <div class="panel-body">
+          <div className="panel-body">
             <form onSubmit={this.onSubmit}>
-              <div class="form-group">
-                <label for="isbn">Name:</label>
-                <input type="text" class="form-control" name="name" value={name} onChange={this.onChange} placeholder="Name" />
+              <div className="form-group">
+                <label htmlFor="isbn">Name:</label>
+                <input type="text" className="form-control" name="name" value={name} onChange={this.onChange} placeholder="Name" />
               </div>
-              <div class="form-group">
-                <label for="title">Password:</label>
-                <input type="password" class="form-control" name="password" value={password} onChange={this.onChange} placeholder="Password" />
+              <div className="form-group">
+                <label htmlFor="title">Password:</label>
+                <input type="password" className="form-control" name="password" value={password} onChange={this.onChange} placeholder="Password" />
               </div>
-              <div class="form-group">
-                <label for="publisher">Email:</label>
-                <input type="email" class="form-control" name="email" value={email} onChange={this.onChange} placeholder="Email Address" />
+              <div className="form-group">
+                <label htmlFor="publisher">Email:</label>
+                <input type="email" className="form-control" name="email" value={email} onChange={this.onChange} placeholder="Email Address" />
               </div>
-              <div class="form-group">
-                <label for="author">GitHubProfile:</label>
-                <input type="text" class="form-control" name="gitHubProfile" value={gitHubProfile} onChange={this.onChange} placeholder="GitHubProfile" />
+              <div className="form-group">
+                <label htmlFor="author">GitHubProfile:</label>
+                <input type="text" className="form-control" name="gitHubProfile" value={gitHubProfile} onChange={this.onChange} placeholder="GitHubProfile" />
               </div>
-              <div class="form-group">
-                <label for="author">TrelloProfile:</label>
-                <input type="text" class="form-control" name="trelloProfile" value={trelloProfile} onChange={this.onChange} placeholder="TrelloProfile" />
+              <div className="form-group">
+                <label htmlFor="author">TrelloProfile:</label>
+                <input type="text" className="form-control" name="trelloProfile" value={trelloProfile} onChange={this.onChange} placeholder="TrelloProfile" />
               </div>
-              <div class="form-group">
-                <label for="author">SlackProfile:</label>
-                <input type="text" class="form-control" name="slackProfile" value={slackProfile} onChange={this.onChange} placeholder="SlackProfile" />
+              <div className="form-group">
+                <label htmlFor="author">SlackProfile:</label>
+                <input type="text" className="form-control" name="slackProfile" value={slackProfile} onChange={this.onChange} placeholder="SlackProfile" />
               </div>
-              <button type="submit" action="/login" class="btn btn-default">Submit</button>
+              <button type="submit" className="btn btn-default">Submit</button>
             </form>
           </div>
         </div>
