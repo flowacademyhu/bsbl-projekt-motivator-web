31d96218874ffe11ca31d60d1449c232ebd9015d
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-05-30 11:26:29 +0200

    Profile test

--- src/main/java/motivator/api/react_app/src/Components/UserProfile.jsx
+++ src/main/java/motivator/api/react_app/src/Components/UserProfile.jsx
@@ -1,1 +1,22 @@
-// /profile
+import React, { Component } from 'react';
+import axios from 'axios';
+import JSON from './userProfiles.json';
+
+class User extends Component {
+  state = {
+    users: JSON
+  }
+
+  render() {
+    const details = this.state.map((user) => {
+      return (
+        <div>
+          <JSON key={user.id} detail={user} />
+        </div>
+      );
+    }
+    )
+  };
+};
+
+export default User;
