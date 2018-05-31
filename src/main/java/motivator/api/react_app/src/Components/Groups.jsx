2dc826ec7f2c9c29035587673e5606b4dfe0fde9
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-05-31 16:09:51 +0200

    Getting started on Groups trigger pop up

--- src/main/java/motivator/api/react_app/src/Components/Groups.jsx
+++ src/main/java/motivator/api/react_app/src/Components/Groups.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import Popup from 'reactjs-popup';
 /* import axios from 'axios'; */
 
 class Groups extends Component {
+  constructor () {
+    super();
+    this.state = {
+      Motivator: {
+        admins: ['Tasi', 'Feri']
+      },
+      PubOrder: {
+        admins: ['Tyson', 'Bruno']
+      }
+    };
+  }
 
-  state = {
-    Motivator: {
-      admins: ['Tasi', 'Feri']
-    },
-    PubOrder: {
-      admins: ['Tyson', 'Bruno']
-    }
-  };
+  /*   getGroupInfo() {
+      var self = this;
+      axios.get(`http://127.0.0.1:8080/current/groups`)
+        .then(function (response) {
+          console.log(response);
+          self.setState({name: response.data.name})
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } */
 
-/*   getGroupInfo() {
-    var self = this;
-    axios.get(`http://127.0.0.1:8080/current/groups`)
-      .then(function (response) {
-        console.log(response);
-        self.setState({name: response.data.name})
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  } */
-
-  render() {
+  render () {
     return (
       <div>
         <h2>Groups of the user:</h2>
@@ -45,6 +48,16 @@ class Groups extends Component {
           })
         }
         <NavLink to='/groups/create'><Button bsStyle='danger'>Create New Group</Button></NavLink>
+        <Popup trigger={<button>GETTING STARTED</button>} position='top left'>
+          {close => (
+            <div>
+              HI, MAN! YOU ARE ABOUT TO START THIS BEATIFUL APPLICATON, AND I WANT TO EXPLAIN YOU SOME THINGS!
+              <a className='close' onClick={close}>
+                &times;
+              </a>
+            </div>
+          )}
+        </Popup>
       </div>
     );
   }
