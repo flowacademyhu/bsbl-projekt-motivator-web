1f16066a9bde312bfcef1cef41c109c29989b0cc
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-06-04 15:05:22 +0200

    Router commented

--- src/main/java/motivator/api/react_app/src/Router.jsx
+++ src/main/java/motivator/api/react_app/src/Router.jsx
@@ -4,23 +4,23 @@ import { ModalFooter } from 'react-bootstrap'; // Components
 import Login from './Components/Login';
 import UserProfile from './Components/UserProfile';
 import UserProfileEdit from './Components/UserProfileEdit'; 
-import GettingStarted from './Components/GettingStarted';
-import Github from './Components/Github';
+/* import GettingStarted from './Components/GettingStarted';
+import Github from './Components/Github'; */
 import Groups from './Components/Groups';
 import GroupsCreate from './Components/GroupsCreate';
-import GroupsEdit from './Components/GroupsEdit';
+/* import GroupsEdit from './Components/GroupsEdit';
 import GroupsMemberEdit from './Components/GroupsMemberEdit';
 import GroupsMemberNew from './Components/GroupsMemberNew';
 import GroupsProfile from './Components/GroupsProfile';
 import PasswordForgotten from './Components/PasswordForgotten';
 import PasswordReset from './Components/PasswordReset';
 import Slack from './Components/Slack';
-import Statistics from './Components/Statistics';
+import Statistics from './Components/Statistics'; */
 import Trello from './Components/Trello';
 import Registration from './Components/Registration';
-import GroupsLine from './Components/Groups/GroupsLine';
+/* import GroupsLine from './Components/Groups/GroupsLine';
 import GroupStatistics from './Components/Statistics/GroupStatistics';
-import UserStatistics from './Components/Statistics/UserStatistics';
+import UserStatistics from './Components/Statistics/UserStatistics'; */
 import Header from './Components/Header';
 
 const Router = () => (
@@ -35,7 +35,7 @@ const Router = () => (
         <Route path='/userProfile' exact component={UserProfile} />
         <Route path='/userProfileEdit' exact component={UserProfileEdit} />
         <Route path='/groupsCreate' component={GroupsCreate} />
-        <Route path='/slack' exact component={Slack} />
+       {/*  <Route path='/slack' exact component={Slack} />
         <Route path='/gettingStarted' component={GettingStarted} />
         <Route path='/github' component={Github} />
         <Route path='/groupsEdit' component={GroupsEdit} />
@@ -44,11 +44,11 @@ const Router = () => (
         <Route path='/groupsProfile' component={GroupsProfile} />
         <Route path='/passwordForgotten' exact component={PasswordForgotten} />
         <Route path='/passwordReset' exact component={PasswordReset} />
-        <Route path='/statistics' exact component={Statistics} />
+        <Route path='/statistics' exact component={Statistics} /> */}
         <Route path='/trello' exact component={Trello} />
-        <Route path='/groupsLine' exact component={GroupsLine} />
+        {/* <Route path='/groupsLine' exact component={GroupsLine} />
         <Route path='/groupStatistics' exact component={GroupStatistics} />
-        <Route path='/userStatistics' exact component={UserStatistics} /> */} */}
+        <Route path='/userStatistics' exact component={UserStatistics} /> */} */} */}
         <Route path='/' exact component={Groups} />
         <Route render={() => <h3>Error 404 Custom Page</h3>} />
       </Switch>
