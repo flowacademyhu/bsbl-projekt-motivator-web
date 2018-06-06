52fddea64b17294663bf64fcb62f97fb531b3a8b
Tóth Máté <mate.toth@flowacademy.hu>
2018-06-06 12:18:22 +0200

    header active group

--- src/main/java/motivator/api/react_app/src/Components/Header.jsx
+++ src/main/java/motivator/api/react_app/src/Components/Header.jsx
@@ -3,15 +3,76 @@ import { NavLink } from 'react-router-dom';
 import { Button, ButtonToolbar } from 'react-bootstrap';
 
 class Header extends Component {
+  state = {
+    Groups: [],
+    ActiveGroup: ''
+  }
+
+  componentWillMount = () => {
+    this.getGroups()
+  }
+
+  redir = (props) => {
+    this.props.history.push('/login');
+  };
+
+  getGroups = () => {
+    var token = window.localStorage.getItem(`Authorization`);
+    var config = {
+      headers: {
+        Authorization: `Bearer ` + token
+      }
+    };
+    var self = this;
+
+    axios.get(`http://127.0.0.1:8080/app/currentuser/activegroup`, config)
+    .then((res) => {
+      if (res.status === 200) {
+        self.setState({Groups: res.data})
+      /* } else {
+        redir() */
+      }
+    })
+  }
+
+  setGroup = () => {
+    var token = window.localStorage.getItem(`Authorization`);
+    var config = {
+      headers: {
+        Authorization: `Bearer ` + token
+      }
+    };
+    var activegroup = this.state.ActiveGroup;
+
+    axios.post(`http://127.0.0.1:8080/app/currentuser/activegroup`, { activegroup }, config)
+    .then((res) => {
+      if (res.status === 200) {
+        console.log('Success')
+      }
+    })
+  }
+
+  handleChange = (event) => {
+    const state = this.state
+    state[event.target.name] = event.target.value;
+    this.setState(state);
+    this.setGroup;
+  }
+
+  iterateGroups = () => {
+    this.state.Groups.map((group) => {
+      return <option value={group}>group</option>
+    })
+  }
+
   render () {
     return (
       <div className='Header'>
         <ButtonToolbar>
           <NavLink to='/'><Button bsStyle='primary'>Home</Button></NavLink>
-          <select bsStyle='danger'>
+          <select bsStyle='danger' onChange={this.handleChange}>
             <option selected disabled>Choose your group</option>
-            <option value='group_one'>Group One</option>
-            <option value='group_two'>Group Two</option>
+            {this.iterateGroups}
           </select>
           <NavLink to='/github'><Button bsStyle='success'>GitHub</Button></NavLink>
           <NavLink to='/slack'><Button bsStyle='info'>Slack</Button></NavLink>
