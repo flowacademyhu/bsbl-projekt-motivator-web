783b2f4cb056c458e0396cc290fa6b554bf044ca
Norbert Muller <mullernorbert93@gmail.com>
2018-06-08 11:44:48 +0200

    add member

--- src/main/java/motivator/api/controllers/GroupController.java
+++ src/main/java/motivator/api/controllers/GroupController.java
@@ -1,7 +1,6 @@
 package motivator.api.controllers;
 
 import io.jsonwebtoken.*;
-import motivator.api.config.HibernateUtil;
 import motivator.api.dao.GroupAdminRepository;
 import motivator.api.dao.GroupRepository;
 import motivator.api.dao.GroupUserRepository;
@@ -11,17 +10,21 @@ import motivator.api.models.User;
 import motivator.api.models.GroupAdmin;
 import motivator.api.service.GroupService;
 import motivator.api.service.UserService;
-import org.hibernate.SQLQuery;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.jsonwebtoken.Jwts;
 
+import java.util.Properties;
+
+/*import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;*/
+
 import javax.naming.NameAlreadyBoundException;
-import java.util.List;
+
 
 @CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RestController
@@ -47,8 +50,6 @@ public class GroupController {
                 .parseClaimsJws(Authorization).getBody();
 
         String email = claims.getSubject();
-        System.err.println(email);
-
         User user = userService.findByEmail(email);
         Group newGroup = new Group();
         newGroup.setName(group.getName());
@@ -60,7 +61,7 @@ public class GroupController {
         newGroup.setSlackGroupHook(group.getSlackGroupHook());
         user.setActiveGroup(newGroup.getName());
         setAdmin(user, newGroup);
-        setUser(user, newGroup);
+        setMember(user, newGroup);
         return groupService.save(newGroup);
 
     }
@@ -73,7 +74,7 @@ public class GroupController {
         return groupAdmin;
     }
 
-    public GroupUser setUser(User user, Group group) {
+    public GroupUser setMember(User user, Group group) {
         GroupUser groupUser = new GroupUser();
         groupUser.setUser(user);
         groupUser.setGroup(group);
@@ -91,12 +92,11 @@ public class GroupController {
         User user = userService.findByEmail(claims.getSubject());
         String activeGroup = user.getActiveGroup();
         Group group = groupService.findByName(activeGroup);
-        System.err.println(group);
         return new ResponseEntity<Group>(group, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/app/currentuser/groups/profile/edit", method = RequestMethod.POST)
-    public ResponseEntity<Group> updateGroup(@RequestHeader (value = "Authorization") String jwtToken, @RequestBody Group group, User userOfGroup) {
+    public ResponseEntity<Group> updateGroup(@RequestHeader (value = "Authorization") String jwtToken, @RequestBody Group group) {
         jwtToken = jwtToken.replace("Bearer ", "");
         Claims claims = Jwts.parser()
                 .setSigningKey("secretkey")
@@ -105,7 +105,6 @@ public class GroupController {
         User user = userService.findByEmail(claims.getSubject());
         String activeGroup = user.getActiveGroup();
         Group groupDb = groupService.findByName(activeGroup);
-        System.err.println(user);
         groupDb.setName(group.getName());
         groupDb.setGitHubGrupRep(group.getGitHubGrupRep());
         groupDb.setTrelloGroup(group.getTrelloGroup());
@@ -113,7 +112,58 @@ public class GroupController {
         groupService.save(groupDb);
         user.setActiveGroup(group.getName());
         userService.save(user);
-        System.err.println(groupDb);
+
         return new ResponseEntity <Group>(groupDb, HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/app/currentuser/groups/profile/edit/new/member", method = RequestMethod.POST)
+    public Group addNewMember(@RequestHeader (value = "Authorization") String jwtToken, @RequestBody String addUser) {
+        jwtToken = jwtToken.replace("Bearer ", "");
+        Claims claims = Jwts.parser()
+                .setSigningKey("secretkey")
+                .parseClaimsJws(jwtToken).getBody();
+
+        User user = userService.findByEmail(claims.getSubject());
+        String activeGroup = user.getActiveGroup();
+        Group groupDb = groupService.findByName(activeGroup);
+        User addNewUser = userService.findByEmail(addUser.split("\"")[3]);
+        setMember(addNewUser, groupDb);
+        System.out.println(addUser.split("\"")[3]);
+        return groupService.save(groupDb);
+    }
+
+    /*private void sendMail(String from, String pw, String to, String subject, String content) {
+
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pw);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        javax.mail.Session session = javax.mail.Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress toAddress = new InternetAddress(to);
+
+            message.addRecipient(javax.mail.Message.RecipientType.TO, toAddress);
+
+            message.setSubject(subject);
+            message.setText(content);
+            javax.mail.Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pw);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (javax.mail.MessagingException me) {
+            me.printStackTrace();
+        }
+    }*/
 }
