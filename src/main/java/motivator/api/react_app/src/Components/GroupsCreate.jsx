1892bc7d41d6d7899505c0b951935de7d7387122
Tóth Máté <mate.toth@flowacademy.hu>
2018-06-08 10:44:09 +0200

    groupcreate react adjustment

--- src/main/java/motivator/api/react_app/src/Components/GroupsCreate.jsx
+++ src/main/java/motivator/api/react_app/src/Components/GroupsCreate.jsx
@@ -8,7 +8,9 @@ class CreateGroup extends Component {
       name: '',
       gitHubGrupRep: '',
       trelloGroup: '',
-      slackGroupHook: ''
+      slackGroupHook: '',
+      owner: '',
+      repoName: ''
     };
   }
 
@@ -20,7 +22,7 @@ class CreateGroup extends Component {
     const state = this.state
     state[e.target.name] = e.target.value;
     this.setState(state);
-};
+  };
 
   onSubmit = (e) => {
     e.preventDefault();
@@ -67,10 +69,8 @@ class CreateGroup extends Component {
                 <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands&client_id=242600405299.364909370132">
                 <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" 
                 srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
-                <label htmlFor="slack">Slack:</label>
-                <input type="text" className="form-control" name="slackGroupHook" value={slackGroupHook} onChange={this.onChange} placeholder="Give us a SlackHook" />
-              </div>
               <button type="submit" className="btn btn-default">Submit</button>
+              </div>
             </form>
           </div>
         </div>
